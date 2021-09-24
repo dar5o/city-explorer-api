3bbaccb09ec336709681cc4b47e7d86df5ba041c
Dario Vitorte <vitortedario@gmail.com>
2021-09-24 14:27:12 -0700

    added cache to api

--- modules/forecastget.js
+++ modules/forecastget.js
@@ -3,27 +3,37 @@
 const axios = require("axios");
 require('dotenv').config();
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const cache = require('./cache.js');
 
-async function weatherHandler(request, response) {
+function weatherHandler(lat, lon) {
+  // let lat = request.query.lat;
+  // let lon = request.query.lon;
+  const key = 'weather-' + lat + lon;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
 
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+      .then(response => parseWeather(response));
+  }
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
   try {
-    let forecastArr = [];
-    let cityName = request.query.cityName;
-    console.log(cityName);
-    let lat = request.query.lat;
-    let lon = request.query.lon;
-    //call to weather api
-    const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily/?city=${cityName}&key=${WEATHER_API_KEY}`);
-    const weatherInfo = weatherData.data.data.map(x => {
-      return new Forecast(x);
+    const weatherSummaries = weatherData.data.data.map(day => {
+      return new Forecast(day);
     });
-    
-    forecastArr.push(weatherInfo);
-    response.send(forecastArr);
-  } catch (error) {
-    response.status(404).send('Something went wrong with the weather data!');
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
   }
 }
+
 
 class Forecast {
   constructor(data) {
--- modules/movieget.js
+++ modules/movieget.js
@@ -2,25 +2,31 @@
 const axios = require("axios");
 require('dotenv').config();
 const MOVIES_API_KEY = process.env.MOVIES_API_KEY;
+let cache = require('./cache.js');
+
 
 async function moviesHandler(request, response) {
   try {
     let cityName = request.query.city;
+    const key = 'movie-' + cityName;
     let resultsArr = [];
     let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${cityName}&api_key=${MOVIES_API_KEY}&language=en-US&page=1&include_adult=false`);
     //movie stuff here
-    console.log(movieData.data);
-    movieData.data.results.map(movie => {
-      resultsArr.push(new Movie(movie))
-    });
-
-    console.log(resultsArr);
-    response.send(resultsArr);
-  } catch (error){
+    if (cache[key] && (Date.now() - cache[key].timestamp < (1000 * 60 * 60 * 24 * 30))) {
+      console.log('Cache hit');
+    } else {
+      console.log('Cache miss. Writing to cache now');
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+      cache[key].data = movieData.data.results.map(movie => new Movie(movie));
+    }
+    response.send(cache[key].data);
+  } catch (error) {
     console.log(error)
     response.status(404).send('Something went wrong with the movie data!');
   }
 }
+
 
 class Movie {
   constructor(data) {
